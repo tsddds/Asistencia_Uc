1927f6ed80d6611a874b2c2a355536c7e4fc85cc
Renato Aguilera <ren.aguilera@duocuc.cl>
2024-10-31 18:59:29 -0300

    base de asistencias modificado, falta integrar logica con db y servicio asistecia fue creado pero no posee data ni funciones

--- src/app/menu/asistencias/asistencias.page.ts
+++ src/app/menu/asistencias/asistencias.page.ts
@@ -1,20 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Asistencia {
+  fecha: string;
+  presente: boolean;
+}
+
+interface Asignatura {
+  nombre: string;
+  asistencias: Asistencia[];
+}
+
+
 @Component({
   selector: 'app-asistencias',
   templateUrl: './asistencias.page.html',
   styleUrls: ['./asistencias.page.scss'],
 })
 export class AsistenciasPage implements OnInit {
-  asistencias = [
-    {asignatura: "PGY4121", fecha: "02/07/2024", presente: true},
-    {asignatura: "PGY2121", fecha: "02/07/2024", presente: true},
-    {asignatura: "mdy3101", fecha: "02/07/2024", presente: false},
-    {asignatura: "ghj9234", fecha: "03/07/2024", presente: false},
-    {asignatura: "dwd4636", fecha: "03/07/2024", presente: true},
-    {asignatura: "fse4234", fecha: "03/07/2024", presente: false},
-    {asignatura: "ffd3123", fecha: "04/07/2024", presente: true},
+
+  asistenciasData: Asignatura[] = [
+    {
+      nombre: "PGY4121",
+      asistencias: [
+        { fecha: "02/07/2024", presente: true },
+        { fecha: "03/07/2024", presente: false },
+      ],
+    },
+    {
+      nombre: "PGY2121",
+      asistencias: [
+        { fecha: "02/07/2024", presente: true },
+      ],
+    },
+    {
+      nombre: "mdy3101",
+      asistencias: [
+        { fecha: "02/07/2024", presente: false },
+        { fecha: "03/07/2024", presente: true },
+      ],
+    },
   ]
+  
   constructor() { }
 
   ngOnInit() {
